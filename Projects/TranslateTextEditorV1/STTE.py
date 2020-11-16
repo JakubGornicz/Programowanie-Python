b07041a75fa94d8e310c9bf1e72d8ca6e112671f
Jakub <gorniczjakub@gmail.com>
2020-11-16 23:31:59 +0100

    "initial commit - fix the bugs"

---
+++ Projects/TranslateTextEditorV1/STTE.py
@@ -0,0 +1,108 @@
+import tkinter as tk
+from tkinter import scrolledtext, ttk
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+from googletrans import Translator
+
+
+def open_file():
+    """Open a file for editing."""
+    global filepath
+    filepath = askopenfilename(
+        filetypes=[("Text Files", "*.txt")]
+    )
+    if not filepath:
+        return
+    txt_edit.delete("1.0", tk.END)
+    with open(filepath, "r") as input_file:
+        text = input_file.read()
+        txt_edit.insert("1.0", text)
+    window.title(f"SimplyEdit - {filepath}")
+
+
+def save_file_as():
+    """Save the current file as a new file."""
+    new_filepath = asksaveasfilename(
+        defaultextension="txt",
+        filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")],
+    )
+    if not new_filepath:
+        return
+    with open(new_filepath, "w") as output_file:
+        text = txt_edit.get("1.0", tk.END)
+        output_file.write(text)
+    window.title(f"SimplyEdit - {new_filepath}")
+
+
+def save_file():
+    if filepath:
+        with open(filepath, "w") as output_file:
+            text = txt_edit.get("1.0", tk.END)
+            output_file.write(text)
+        window.title(f"SimplyEdit - {filepath}")
+
+
+def trans():
+    translator = Translator()
+    for x in txt_edit.get('1.0', 'end-1c').splitlines():
+        lines.append(x)
+        f = str(from_lang.get())
+        t = str(to_lang.get())
+        result = translator.translate(text=x, src=f, dest=t)
+        s = str(str(result.text) + ' - ' + str(x)+'\n')
+        tablica_Ready.append(s)
+    txt_edit.delete("1.0", tk.END)
+    print(tablica_Ready)
+    for x in tablica_Ready:
+        txt_edit.insert(2.0, str(x))
+
+
+window = tk.Tk()
+
+lines = []
+tablica_Ready = []
+window.title("SimplyEdit")
+
+window.rowconfigure(0, minsize=800, weight=1)
+window.columnconfigure(1, minsize=800, weight=1)
+
+txt_edit = scrolledtext.ScrolledText(master=window, font=("Helvetica", 14))
+fr_buttons = tk.Frame(master=window, relief=tk.SUNKEN, borderwidth=2)
+
+btn_open = tk.Button(master=fr_buttons, text="Open", command=open_file)
+btn_save_as = tk.Button(master=fr_buttons, text="Save As...", command=save_file_as)
+btn_save = tk.Button(master=fr_buttons, text="Save", command=save_file)
+btn_trans = tk.Button(master=fr_buttons, text="Translate")
+
+btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+btn_save.grid(row=1, column=0, sticky="ew", padx=5)
+btn_save_as.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+btn_trans.grid(row=3, column=0, sticky="ew", padx=5, pady=10)
+
+fr_buttons.grid(row=0, column=0, sticky="ns")
+txt_edit.grid(row=0, column=1, sticky="nsew")
+
+translation_mode = tk.Tk()
+translation_mode.geometry('375x75')
+translation_mode.title("Chose translation mode")
+
+lbl_select = ttk.Label(master=translation_mode, text="Select:", font=("Helvetica", 11))
+
+n = tk.StringVar()
+m = tk.StringVar()
+
+from_lang = ttk.Combobox(master=translation_mode, width=13, textvariable=n, state='readonly')
+from_lang['values'] = ['en', 'pl', 'no']
+from_lang.current(1)
+to_lang = ttk.Combobox(master=translation_mode, width=13, textvariable=m, state='readonly')
+to_lang['values'] = ['en', 'pl', 'no']
+to_lang.current(1)
+lbl_select.grid(column=0, row=5, padx=10, pady=25)
+btn_submit = ttk.Button(master=translation_mode, text="submit", command=trans)
+btn_submit.grid(column=3, row=5, padx=10, pady=25)
+
+from_lang.grid(column=1, row=5)
+to_lang.grid(column=2, row=5)
+
+translation_mode.mainloop()
+
+window.mainloop()
