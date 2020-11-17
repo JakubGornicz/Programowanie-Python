48e466a6405e67a72953337afd3ccb4d11d43021
Jakub <gorniczjakub@gmail.com>
2020-11-17 20:16:23 +0100

    "cleaned up redundant code, fixed a bug"

--- Projects/TranslatingTextEditor/STTE.py
+++ Projects/TranslatingTextEditor/STTE.py
@@ -16,7 +16,7 @@ def open_file():
     with open(filepath, "r") as input_file:
         text = input_file.read()
         txt_edit.insert("1.0", text)
-    window.title(f"SimplyEdit - {filepath}")
+    window.title(f"STTE - {filepath}")
 
 
 def save_file_as():
@@ -30,15 +30,16 @@ def save_file_as():
     with open(new_filepath, "w") as output_file:
         text = txt_edit.get("1.0", tk.END)
         output_file.write(text)
-    window.title(f"SimplyEdit - {new_filepath}")
+    window.title(f"STTE - {new_filepath}")
 
 
 def save_file():
+    """Save the contents in a OPENED file"""
     if filepath:
         with open(filepath, "w") as output_file:
             text = txt_edit.get("1.0", tk.END)
             output_file.write(text)
-        window.title(f"SimplyEdit - {filepath}")
+        window.title(f"STTE - {filepath}")
 
 
 def trans():
@@ -51,14 +52,10 @@ def trans():
         if len(word) > 0:
             txt_edit.insert(f"{i}.{str(len(word))}", f" - {result}")
         i += 1
-    translation_mode.destroy()
 
 
 window = tk.Tk()
-
-translated = []
-tablica_Ready = []
-window.title("SimplyEdit")
+window.title("STTE")
 
 window.rowconfigure(0, minsize=780, weight=1)
 window.rowconfigure(1, minsize=20, weight=1)
@@ -70,20 +67,18 @@ fr_buttons = tk.Frame(master=window, relief=tk.SUNKEN, borderwidth=2)
 btn_open = tk.Button(master=fr_buttons, text="Open", command=open_file)
 btn_save_as = tk.Button(master=fr_buttons, text="Save As...", command=save_file_as)
 btn_save = tk.Button(master=fr_buttons, text="Save", command=save_file)
-btn_trans = tk.Button(master=fr_buttons, text="Translate", command=trans)
 
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5)
 btn_save_as.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
-btn_trans.grid(row=3, column=0, sticky="ew", padx=5, pady=10)
 
 fr_buttons.grid(row=0, column=0, sticky="ns")
 txt_edit.grid(row=0, column=1, sticky="nsew")
 
 translation_mode = tk.Frame(master=window)
 
-lbl_description = tk.Label(master=translation_mode, text="Chose translation mode: ", font=("Helvetica", 13))
-lbl_select = ttk.Label(master=translation_mode, text="Select:", font=("Helvetica", 11))
+lbl_description = tk.Label(master=translation_mode, text="Chose translation:", font=("Helvetica", 13))
+btn_trans = tk.Button(master=translation_mode, text="Translate", command=trans)
 
 from_lang = ttk.Combobox(master=translation_mode, width=13)
 from_lang['values'] = ['en', 'pl', 'no']
@@ -93,9 +88,10 @@ to_lang = ttk.Combobox(master=translation_mode, width=13)
 to_lang['values'] = ['en', 'pl', 'no']
 to_lang.current()
 
-lbl_description.grid(column=0, row=5, padx=10, pady=3)
+lbl_description.grid(column=0, row=5, padx=10, pady=10)
 from_lang.grid(column=1, row=5)
-to_lang.grid(column=2, row=5, padx=20)
+to_lang.grid(column=2, row=5, padx=5)
+btn_trans.grid(column=3, row=5, sticky="ew", padx=20)
 
 translation_mode.grid(row=1, column=1, sticky="e")
 
