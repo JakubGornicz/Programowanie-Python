e8edd619b5f0d67eb68520f9a2c6c8d40b45eb92
Jakub <gorniczjakub@gmail.com>
2020-11-25 00:36:06 +0100

    garbage

--- other/dots to comma.py
+++ other/dots to comma.py
@@ -1,3 +1,5 @@
-dots = "4.4  3.8  4.4  3.6  3.3  4.5"
-print(dots.replace('.', ',').replace('  ', '\n'))
+import googletrans
+# dots = "4.4  3.8  4.4  3.6  3.3  4.5"
+# print(dots.replace('.', ',').replace('  ', '\n'))
+
 
