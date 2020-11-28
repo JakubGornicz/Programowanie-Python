59c9158bec15ccb3599e0474cf0e3ca503affc1a
Jakub <gorniczjakub@gmail.com>
2020-11-28 21:47:16 +0100

    final commit

--- AiSD/Graph.py
+++ AiSD/Graph.py
@@ -1,6 +1,7 @@
 from enum import Enum
 from typing import Any, Optional, Dict, List
-
+import networkx as nx
+import matplotlib.pyplot as plt
 
 class EdgeType(Enum):
     directed = 1
@@ -30,12 +31,13 @@ class Edge(object):
         self.weight = weight
 
     def __repr__(self):
-        return f"{self.destination.index} : {self.destination.data}"
+        return f"{self.destination.index}: {self.destination.data}"
 
 
 class Graph(object):
     graph_dict: Dict[Vertex, List[Edge]]
     weight: bool
+    edge_weights: Dict[tuple, int]
 
     def __init__(self, weight=False):
         self.graph_dict = dict()
@@ -78,16 +80,83 @@ class Graph(object):
         else:
             return
 
+    def traverse_breadth_first(self, visited=None) -> None:
+        if visited is None:
+            visited = []
+        queue = []
+        start = list(self.graph_dict.keys())[0]
+        visited.append(start)
+        queue.append(start)
+        while queue:
+            current = queue.pop(0)
+
+            for neighbour in self.graph_dict[current]:
+                if neighbour.destination not in visited:
+                    visited.append(neighbour.destination)
+                    queue.append(neighbour.destination)
+        print(visited)
+
+    def dfs(self, v: Vertex, visited: List[Vertex]):
+        visited.append(v)
+        for neighbour in self.graph_dict[v]:
+            if neighbour.destination not in visited:
+                self.dfs(neighbour.destination, visited)
+
+    def traverse_depth_first(self, visited=None) -> None:
+        if visited is None:
+            visited = []
+        start = list(self.graph_dict.keys())[0]
+        self.dfs(start, visited)
+        print(visited)
+
+    def show(self):
+        graph = nx.DiGraph()
+        start = list(self.graph_dict.keys())[0]
+        queue = [start]
+        visited = [start]
+        edge_weights = {}
+        while queue:
+            current = queue.pop(0)
+            for neighbour in self.graph_dict[current]:
+                graph.add_edge(current.data, neighbour.destination.data)
+                curr_weight = {(current.data, neighbour.destination.data): neighbour.weight}
+                edge_weights.update(curr_weight)
+                if neighbour.destination not in visited:
+                    visited.append(neighbour.destination)
+                    queue.append(neighbour.destination)
+                    graph.add_edge(current.data, neighbour.destination.data)
+
+        pos = nx.spectral_layout(graph)
+        nx.draw(graph, pos, edge_color='black', width=1, linewidths=1, node_size=3000, node_color='pink', alpha=0.7,
+                labels={node: node for node in graph.nodes()})
+        if self.weight is True:
+            nx.draw_networkx_edge_labels(graph, pos, edge_labels=edge_weights, font_color='red')
+        plt.axis('off')
+        plt.show()
+
 
 if __name__ == "__main__":
 
-    g = Graph()
-    v1 = g.create_vertex("Stavanger")
-    v2 = g.create_vertex("New York")
-    g.add_directed_edge(g.create_vertex("Katowice"), v1)
-    g.add_undirected_edge(v1, g.create_vertex("Warszawa"))
-    g.add(EdgeType(1), v2, g.create_vertex("Washington"))
-    g.add(EdgeType(2), v2, g.create_vertex("Los Angeles"))
+    g = Graph(weight=True)
+    v0 = g.create_vertex("v0")
+    v1 = g.create_vertex("v1")
+    v2 = g.create_vertex("v2")
+    v3 = g.create_vertex("v3")
+    v4 = g.create_vertex("v4")
+    v5 = g.create_vertex("v5")
+
+    g.add_directed_edge(v0, v1, 10)
+    g.add_directed_edge(v0, v5, 5)
+    g.add_directed_edge(v5, v2, 20)
+    g.add_directed_edge(v2, v1, 9)
+    g.add_directed_edge(v2, v3, 4)
+    g.add_directed_edge(v3, v4, 3)
+    g.add_directed_edge(v4, v1, 10)
+    g.add_directed_edge(v4, v5, 3)
 
     print(g.graph_dict)
     print(g)
+
+    g.traverse_breadth_first()
+    g.traverse_depth_first()
+    g.show()
